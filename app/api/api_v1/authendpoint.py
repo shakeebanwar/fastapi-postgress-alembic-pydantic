1565c289318929dcc87ace119188d68fd6a64b1d
shakeebanwar <shakeebanwar250@gmail.com>
2023-07-29 06:08:52 -0700

    backup

--- app/api/api_v1/authendpoint.py
+++ app/api/api_v1/authendpoint.py
@@ -1,19 +1,24 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 from app import crud, schemas
 from app.models import User
-from app.database import SessionLocal
-from typing import List
 from app.database import get_db
 
 router = APIRouter()
 
-
-@router.post("/", status_code=201, response_model=schemas.UserRead)
+@router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.UserRead)
 def registerUser(user_data: schemas.UserBase, db: Session = Depends(get_db)):
     existing_user = crud.get_user_by_email(db, email=user_data.email)
     if existing_user:
-        raise HTTPException(status_code=400, detail="Email already registered")
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Email already registered")
 
     new_user = crud.create_user(db, user_data)
     return new_user
+
+@router.post("/login/")
+def login_user(login_data: schemas.UserBase, db: Session = Depends(get_db)):
+    user = crud.get_user_by_email(db, email=login_data.email)
+    if not user or not crud.verify_password(login_data.password, user.password):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials")
+
+    return {"status": True, "message": "Login Successfully", "data": {"id": user.id, "email": user.email}}
--- app/crud.py
+++ app/crud.py
@@ -1,6 +1,7 @@
 from sqlalchemy.orm import Session
 from app.models import Item,User
 from app.schemas import ItemCreate, ItemUpdate,UserBase
+import bcrypt
 
 
 #items
@@ -48,3 +49,8 @@ def create_user(db: Session, user_data: UserBase) -> User:
     db.refresh(new_user)
 
     return new_user
+
+
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return bcrypt.checkpw(plain_password.encode('utf-8'), hashed_password.encode('utf-8'))
